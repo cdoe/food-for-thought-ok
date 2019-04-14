39018e6f9ff50202a2f5a157e88da8049c431195
cdoe <cdoe@users.noreply.github.com>
2019-04-13 19:20:43 -0500

    Added first pass of leaflet map integration

--- src/pages/LocationsPage.tsx
+++ src/pages/LocationsPage.tsx
@@ -3,6 +3,7 @@ import React, { FunctionComponent, Fragment, useState, useRef, useEffect } from 
 import { RouteComponentProps } from 'react-router';
 import { Link } from 'react-router-dom';
 import classnames from 'classnames';
+import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 // Styles
 import './LocationsPage.scss';
 // Components
@@ -42,13 +43,27 @@ const OpenNowFilter: FunctionComponent = () => {
   );
 };
 
+// declare global {
+//   interface Window {
+//     L: L;
+//   }
+// }
+
 // Component
 const LocationsPage: FunctionComponent<RouteComponentProps<{ locationId?: string }>> = ({
+  history,
   match
 }) => {
+  const retina = typeof window !== 'undefined' && window.devicePixelRatio >= 2;
   // Mobile list and details states
   const [mobileListIsOpen, setMobileListIsOpen] = useState(false);
   const [mobileDetailsExpanded, setMobileDetailsExpanded] = useState(false);
+  // const [leaflet, setLeaflet] = useState({});
+  // Default to OKC
+  const defaultMapCenter: [number, number] = [35.4823242, -97.7593689];
+  const defaultMapZoom = 7;
+  const [mapCenter, setMapCenter] = useState(defaultMapCenter);
+  const [mapZoom, setMapZoom] = useState(defaultMapZoom);
 
   // Get locationId from URL (if exists)
   const locationIdParam = match.params.locationId;
@@ -57,8 +72,20 @@ const LocationsPage: FunctionComponent<RouteComponentProps<{ locationId?: string
 
   // Effect triggered when locationId in URL changes
   useEffect(() => {
-    // If location id is present in URL, set locationId state
-    !!locationIdParam && setLocationId(locationIdParam);
+    // If location id is present in URL (and is valid location), set locationId state
+    // and center map at that location
+    if (!!locationIdParam) {
+      const location = locations.find(location => location.id === locationIdParam);
+      if (location && location.lat && location.lng) {
+        setLocationId(locationIdParam);
+        setMapCenter([parseFloat(location.lat), parseFloat(location.lng)]);
+        setMapZoom(12);
+      } else {
+        // If couldn't find location, set map to default settings
+        setMapCenter(defaultMapCenter);
+        setMapZoom(defaultMapZoom);
+      }
+    }
     // Close mobile list view if showing location details
     // !!locationIdParam && setMobileListIsOpen(false);
 
@@ -81,6 +108,25 @@ const LocationsPage: FunctionComponent<RouteComponentProps<{ locationId?: string
       locationsListRef.current.scrollTop = 58;
     }
   }, []);
+
+  // On load, load leaflet JS and map tiles into div#leaflet-map container
+  // useEffect(() => {
+  // var map = L.map('leaflet-map').setView([36.1522199, -96.0180911], 11);
+  // L.tileLayer(
+  //   'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiY2RvZSIsImEiOiJjanVmeGRmaGowZjM3NDlucXNocmZyZmZrIn0.zW5aLSTzrHl5OAx4YF4ETA',
+  //   {
+  //     attribution:
+  //       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  //     maxZoom: 18
+  //     // id: 'mapbox.streets',
+  //     // accessToken: 'pk.eyJ1IjoiY2RvZSIsImEiOiJjanVmeGRmaGowZjM3NDlucXNocmZyZmZrIn0.zW5aLSTzrHl5OAx4YF4ETA'
+  //   }
+  // ).addTo(map);
+  // }, []);
+
+  // const handleLocationSelect = (e: MouseEvent) => {
+  //   console.log('e', e);
+  // };
 
   return (
     <Fragment>
@@ -125,7 +171,38 @@ const LocationsPage: FunctionComponent<RouteComponentProps<{ locationId?: string
           </div>
 
           {/* Embedded map */}
-          <div className="map-embed">Map Placeholder</div>
+          {/* <div id="leaflet-map" className="map-embed">
+            Map Placeholder
+          </div> */}
+          <Map className="map-embed" center={mapCenter} zoom={mapZoom} animate={true}>
+            <TileLayer
+              access_token="pk.eyJ1IjoiY2RvZSIsImEiOiJjanVmeGRmaGowZjM3NDlucXNocmZyZmZrIn0.zW5aLSTzrHl5OAx4YF4ETA"
+              id="mapbox.streets"
+              subdomains="abcd"
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url={`https://{s}.basemaps.cartocdn.com/rastertiles/light_all/{z}/{x}/{y}${
+                retina ? '@2x' : ''
+              }.png`}
+              // url="https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={access_token}"
+            />
+            {locations.map(location => {
+              if (location.lat && location.lng) {
+                const latLng: [number, number] = [
+                  parseFloat(location.lat),
+                  parseFloat(location.lng)
+                ];
+                return (
+                  <Marker
+                    key={location.id}
+                    position={latLng}
+                    onClick={() => {
+                      history.push('/locations/' + location.id);
+                    }}
+                  />
+                );
+              }
+            })}
+          </Map>
           {/* Location detail panel */}
           <div
             className={classnames('location-detail-panel', {
