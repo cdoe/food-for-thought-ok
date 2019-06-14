082b3f2a1055992a5390afe8da72ca74174d18c8
cdoe <cdoe@users.noreply.github.com>
2019-06-13 21:14:39 -0500

    Domain path ending in '/es' sets language to spanish

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -10,9 +10,7 @@ import LocationsPage from './pages/LocationsPage';
 import FaqPage from './pages/FaqPage';
 import ContactPage from './pages/ContactPage';
 import ListPage from './pages/ListPage';
-import NondiscriminationPage, {
-  NondiscriminationPageSpanish
-} from './pages/NondiscriminationPage';
+import NondiscriminationPage, { NondiscriminationPageSpanish } from './pages/NondiscriminationPage';
 // Layout Components
 import PageWrapper from './layout/PageWrapper';
 import PrimaryNav from './layout/PrimaryNav';
@@ -21,37 +19,38 @@ import { useTranslation } from 'react-i18next';
 import withGaTracker from './lib/withGaTracker';
 
 // Render very top-level App component
-const AppRoutes: FunctionComponent = () => {
-  const { t } = useTranslation();
+const AppRoutes: FunctionComponent = props => {
+  const { t, i18n } = useTranslation();
 
   return (
     <Switch>
+      {/* Set https://[domain]/es to spanish  */}
+      <Route
+        path="/es"
+        exact
+        render={() => {
+          i18n.language !== 'es' && i18n.changeLanguage('es');
+          return <Redirect to="/" />;
+        }}
+      />
       <PageWrapper>
         <PrimaryNav />
         <MobileNav />
         <Switch>
           <Route path="/" exact component={withGaTracker(LandingPage)} />
-          <Route
-            path="/locations/:locationId?"
-            component={withGaTracker(LocationsPage)}
-          />
+          <Route path="/locations/:locationId?" component={withGaTracker(LocationsPage)} />
           <Route path="/faq" component={withGaTracker(FaqPage)} />
           <Route path="/contact" component={withGaTracker(ContactPage)} />
           <Route path="/list" component={withGaTracker(ListPage)} />
           {t('footer.nondiscriminationLink') === '/nodiscriminacion' && (
             <Redirect from="/nondiscrimination" to="/nodiscriminacion" />
           )}
-          <Route
-            path="/nondiscrimination"
-            component={withGaTracker(NondiscriminationPage)}
-          />
+          <Route path="/nondiscrimination" component={withGaTracker(NondiscriminationPage)} />
           {t('footer.nondiscriminationLink') === '/nondiscrimination' && (
             <Redirect from="/nodiscriminacion" to="/nondiscrimination" />
           )}
-          <Route
-            path="/nodiscriminacion"
-            component={withGaTracker(NondiscriminationPageSpanish)}
-          />
+          <Route path="/nodiscriminacion" component={withGaTracker(NondiscriminationPageSpanish)} />
+
           <Redirect path="/" to="/" />
         </Switch>
       </PageWrapper>
